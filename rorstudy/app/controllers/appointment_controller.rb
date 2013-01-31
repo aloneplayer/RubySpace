4eaeb4e7249a2e3ef0f8a1c686c25312fc88cb9a
Benjamin Huang <aloneplayer@gmail.com>
2013-01-31 18:55:58 -0500

    Add list action and view.

--- rorstudy/app/controllers/appointment_controller.rb
+++ rorstudy/app/controllers/appointment_controller.rb
@@ -1,12 +1,18 @@
 class AppointmentController < ApplicationController
   def list
-    render :text => "List"
+    #render :text => "List"
+     @appointments = Appointment.all
   end
   
   def show
+    render :text => "Show"
+  end
+  
+  def delete
+    render :text => "Delete"
   end
   
   def create
-    render :text => "Create"
+    #render :text => "Create"
   end
 end
