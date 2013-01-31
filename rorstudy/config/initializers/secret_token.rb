56eb98d7f219bc8c0ece024ea7a7e17d46b5d275
Benjamin Huang <aloneplayer@gmail.com>
2013-01-31 17:00:13 -0500

    Create project

---
+++ rorstudy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rorstudy::Application.config.secret_token = '6dc1c1ea171983eead9dbe5eaa7d3d51d6f259dd63a8456a84978ece8a69d04d3153e80539d5f0742591b844db24cf79fd4a6fd8419d0c992f1d7e33ea2fad38'
