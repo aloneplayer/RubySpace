1065624bda6ffc3989f026954f76b2bd9c205439
Benjamin Huang <aloneplayer@gmail.com>
2013-02-02 16:59:05 -0500

    Update

--- rorstudy/app/controllers/appointment_controller.rb
+++ rorstudy/app/controllers/appointment_controller.rb
@@ -1,38 +1,56 @@
 class AppointmentController < ApplicationController
-  def list
-    #render :text => "List"
-     @appointments = Appointment.all
-  end
-  
-  def show
-    @title="Appointment datail"
-    @appointment = Appointment.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-        flash[:notice] = "Wrong id"
-        redirect_to :action => 'notfound'
-  end
-  
-  def delete
-    @appointment = Appointment.find(params[:id])
-    @appointment.destroy
-    redirect_to :action => "list"
-    rescue ActiveRecord::RecordNotFound
-        flash[:notice] = "Wrong id"
-        redirect_to :action => 'notfound'
-  end
-  
-  def new
-    #render :text => "new"
-    @appointment = Appointment.new
-  end
-  
-  def create
-    #render :text => "Create"
-    @appointment= Appointment.new(params[:appointment])
-    if @appointment.save
-      render 'success' 
-    else
-      render 'new'
+    def list
+        #render :text => "List"
+         @appointments = Appointment.all
     end
-  end
+  
+    def show
+        @title="Appointment datail"
+        @appointment = Appointment.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+            flash[:notice] = "Wrong id"
+            redirect_to :action => 'notfound'
+    end
+  
+    def delete
+        @appointment = Appointment.find(params[:id])
+        @appointment.destroy
+        redirect_to :action => "list"
+        rescue ActiveRecord::RecordNotFound
+            flash[:notice] = "Wrong id"
+            redirect_to :action => 'notfound'
+    end
+  
+    def edit
+        @appointment = Appointment.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+            flash[:notice] = "Wrong id"
+            redirect_to :action => 'notfound'
+    end
+
+    def update
+        @appointment = Appointment.find(params[:id])
+
+        if @appointment.update_attributes(params[:appointment])
+            #redirect_to :action => :show, :id => @appointment
+            redirect_to :action => :list
+        else
+            redirect_to action:edit
+        end
+    end
+  
+    def new
+        #render :text => "new"
+        @appointment = Appointment.new
+    end
+  
+    def create
+        #render :text => "Create"
+        @appointment= Appointment.new(params[:appointment])
+        if @appointment.save
+          render 'success' 
+        else
+          render 'new'
+        end
+    end
  end
