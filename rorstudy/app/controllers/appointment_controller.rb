b9fba61a87b95498b0f2f749af47a346046dbef9
Benjamin Huang <aloneplayer@gmail.com>
2013-02-02 09:18:42 -0500

    Sample form

--- rorstudy/app/controllers/appointment_controller.rb
+++ rorstudy/app/controllers/appointment_controller.rb
@@ -10,9 +10,25 @@ class AppointmentController < ApplicationController
   
   def delete
     render :text => "Delete"
+    @appoiontment = Appointment.find(params[:id])
+    @appoiontment.destroy
+
+    redirect_to action: :index
+  end
+  
+  def new
+    #render :text => "new"
+    @appointment = Appointment.new
   end
   
   def create
     #render :text => "Create"
+    #@appointment= Appointment.new(params[:post].permit(:name, :address, :email, :phone))
+
+    #if @post.save
+    #  redirect_to action: :show, id: @post.id
+    #else
+    #  render 'new'
+    #end
   end
 end
